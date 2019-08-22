7baae301c8c2ca99ca9e80497b2d80d6f5fe2735
Adam Moyer <apmoyer@uw.edu>
2019-08-22 10:32:16 -0700

    Added simple pip install script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aimnet',
+      version='0.1',
+      description='Atoms In Molecules Neural Network Potential',
+      url='https://github.com/aiqm/aimnet.git',
+      author='Roman Zubatyuk',
+      license='MIT',
+      packages=['aimnet'],
+      include_package_data=True,
+      zip_safe=False)
